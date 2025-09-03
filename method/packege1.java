b095e8d7dc4a02fb5168cd5e3f2c0339fcda4274
omkarwani2060 <omkarwani583@gmail.com>
2025-09-03 20:01:28 +0530

    import packege file

--- method/packege1.java
+++ method/packege1.java
@@ -1,4 +1,11 @@
 package method;
 
 public class packege1 {
+    public static void main(String[] args) {
+        second obj = new second();
+        int a = 10;
+        int b = 20;
+        int store = obj.add(a,b);
+        System.out.println("Sum of Two number: " + store);
+    }
 }
